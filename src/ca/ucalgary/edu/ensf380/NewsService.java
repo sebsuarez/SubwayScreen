1366a925faa052e0dcebbcdb5c1f5900b681b19a
Charlie Lang <charlie.lang@ucalgary.ca>
2023-07-30 09:06:20 -0600

    News and weathe files updated and moved to main project

---
+++ src/ca/ucalgary/edu/ensf380/NewsService.java
@@ -0,0 +1,69 @@
+package ca.ucalgary.edu.ensf380;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class NewsService {
+    private static final String API_KEY = "b7358c163a22492e9d29f85397807fde";
+
+
+    public static List<NewsItem> getNews(String keyword) throws IOException {
+        List<NewsItem> newsList = new ArrayList<>();
+
+        try {
+            String url = "https://newsapi.org/v2/top-headlines?country=ca&q=" + keyword + "&apiKey=" + API_KEY;
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                newsList = parseJsonResponse(jsonResponse);
+            } else {
+                throw new IOException("Failed to fetch news data. HTTP error code: " + responseCode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return newsList;
+    }
+
+    private static List<NewsItem> parseJsonResponse(String jsonResponse) {
+        List<NewsItem> newsItems = new ArrayList<>();
+
+        Pattern titlePattern = Pattern.compile("\"title\"\\s*:\\s*\"([^\"]+)\"");
+        Pattern sourceNamePattern = Pattern.compile("\"name\"\\s*:\\s*\"([^\"]+)\"");
+
+        Matcher titleMatcher = titlePattern.matcher(jsonResponse);
+        Matcher sourceNameMatcher = sourceNamePattern.matcher(jsonResponse);
+
+        while (titleMatcher.find() && sourceNameMatcher.find()) {
+            String title = titleMatcher.group(1);
+            String sourceName = sourceNameMatcher.group(1);
+            newsItems.add(new NewsItem(title, sourceName));
+        }
+
+        return newsItems;
+    }
+}
+
+
---
+++ src/ca/ucalgary/edu/ensf380/WeatherService.java
@@ -0,0 +1,84 @@
+package ca.ucalgary.edu.ensf380;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class WeatherService {
+    public WeatherService() {
+    }
+
+    public static WeatherInstance getCurrentWeather() {
+        WeatherInstance currentWeather = new WeatherInstance();
+        String apiKey = "7919cef0282916061e569b2bbca43b3f";
+        String cityCode = "5913490";
+
+        try {
+            String url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityCode + "&appid=" + apiKey + "&units=metric";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String html = response.toString();
+
+                String dateRegex = "\"dt\":(\\d+)";
+                String temperatureRegex = "\"temp\":(\\d+\\.\\d+)";
+                String weatherConditionRegex = "\"description\":\"([^\"]+)\"";
+                String humidityRegex = "\"humidity\":(\\d+)";
+                String windSpeedRegex = "\"speed\":(\\d+\\.\\d+)";
+
+                Pattern datePattern = Pattern.compile(dateRegex);
+                Pattern temperaturePattern = Pattern.compile(temperatureRegex);
+                Pattern weatherConditionPattern = Pattern.compile(weatherConditionRegex);
+                Pattern humidityPattern = Pattern.compile(humidityRegex);
+                Pattern windSpeedPattern = Pattern.compile(windSpeedRegex);
+
+                Matcher dateMatcher = datePattern.matcher(html);
+                Matcher temperatureMatcher = temperaturePattern.matcher(html);
+                Matcher weatherConditionMatcher = weatherConditionPattern.matcher(html);
+                Matcher humidityMatcher = humidityPattern.matcher(html);
+                Matcher windSpeedMatcher = windSpeedPattern.matcher(html);
+
+                if (dateMatcher.find() && temperatureMatcher.find() && weatherConditionMatcher.find() &&
+                        humidityMatcher.find() && windSpeedMatcher.find()) {
+                    long timestamp = Long.parseLong(dateMatcher.group(1));
+                    double temperature = Double.parseDouble(temperatureMatcher.group(1));
+                    String weatherCondition = weatherConditionMatcher.group(1);
+                    int humidity = Integer.parseInt(humidityMatcher.group(1));
+                    double windSpeed = Double.parseDouble(windSpeedMatcher.group(1));
+
+                    windSpeed *= 3.6;
+                    java.util.Date dateTime = new java.util.Date(timestamp * 1000);
+                    java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("MMMM d' 'yyyy, hh:mm a");
+                    String formattedDateTime = sdf.format(dateTime);
+
+                    currentWeather = new WeatherInstance(formattedDateTime, temperature, weatherCondition, humidity, windSpeed);
+                } else {
+                    System.out.println("Error: Unable to parse weather data from the HTML response.");
+                }
+            } else {
+                System.out.println("Error: Unable to fetch weather data. Please check the city code and API key.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return currentWeather;
+    }
+}
+
+ 
